57a3a3f4dcf30e0f3a523992f62f64e1e857d0f5
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-08-09 18:55:29 -0300

    se importa el nuevo router en app.js

--- src/app.js
+++ src/app.js
@@ -1,39 +1,42 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import {Server} from 'socket.io';
+import { Server } from 'socket.io';
 import mongoose from 'mongoose';
 
 import productRouter from './routes/productRouter.js';
 import cartRouter from './routes/cartRouter.js';
 import viewsRouter from './routes/viewsRouter.js';
+import userRouter from './routes/userRouter.js'; 
 import __dirname from './utils/constantsUtil.js';
 import websocket from './websocket.js';
 
 const app = express();
 
 const uri = 'mongodb://127.0.0.1:27017/entrega-final';
-mongoose.connect(uri);
+mongoose.connect(uri)
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error de conexión a MongoDB:', err));
 
-//Handlebars Config
+// Configuración de Handlebars
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/../views');
 app.set('view engine', 'handlebars');
 
-//Middlewares
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//Routers
+// Routers
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
+app.use('/api/users', userRouter); // <-- Nueva ruta para usuarios
 app.use('/', viewsRouter);
 
 const PORT = 8080;
 const httpServer = app.listen(PORT, () => {
-    console.log(`Start server in PORT ${PORT}`);
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
 
 const io = new Server(httpServer);
-
 websocket(io);
