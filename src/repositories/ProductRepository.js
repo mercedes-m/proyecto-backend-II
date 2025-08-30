add83e6025216d098544837c235804a6d9b24fbe
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-08-30 18:14:16 -0300

    se crea el archivo ProductRepository.js

---
+++ src/repositories/ProductRepository.js
@@ -0,0 +1,23 @@
+import { Product } from '../dao/models/Product.js';
+
+export class ProductRepository {
+  async getAll() {
+    return await Product.find();
+  }
+
+  async getById(id) {
+    return await Product.findById(id);
+  }
+
+  async create(productData) {
+    return await Product.create(productData);
+  }
+
+  async update(id, updateData) {
+    return await Product.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  async delete(id) {
+    return await Product.findByIdAndDelete(id);
+  }
+}
