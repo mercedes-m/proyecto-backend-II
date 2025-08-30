f65c8bb6214deff243439889eedeee94c5ca09f3
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-08-30 18:11:53 -0300

    se crea el archivo UserRepository.js

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,27 @@
+import { User } from '../dao/models/User.js';
+
+export class UserRepository {
+  async getAll() {
+    return await User.find();
+  }
+
+  async getById(id) {
+    return await User.findById(id);
+  }
+
+  async getByEmail(email) {
+    return await User.findOne({ email });
+  }
+
+  async create(userData) {
+    return await User.create(userData);
+  }
+
+  async update(id, updateData) {
+    return await User.findByIdAndUpdate(id, updateData, { new: true });
+  }
+
+  async delete(id) {
+    return await User.findByIdAndDelete(id);
+  }
+}
