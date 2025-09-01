52b757bed23e66ffedebed08f51f25fc00481bce
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-01 18:38:42 -0300

    se modifica el código de acuerdo a la arquitectura solicitada

--- src/dao/models/cartModel.js
+++ src/dao/models/cartModel.js
@@ -3,21 +3,22 @@ import mongoose from 'mongoose';
 const cartCollection = 'carts';
 
 const cartSchema = new mongoose.Schema({
-    products: {
-        type: [
-            {
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "products"
-                },
-                quantity: {
-                    type: Number,
-                    default: 1
-                }
-            }
-        ],
-        default: []
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'products',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1
+      }
     }
-});
+  ]
+}, { timestamps: true });
 
-export const cartModel = mongoose.model(cartCollection, cartSchema);
+const CartModel = mongoose.model(cartCollection, cartSchema);
+
+export default CartModel;
