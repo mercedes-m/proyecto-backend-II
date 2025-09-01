86642f841d439d78b7941d39d32763fb7790cc20
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-01 19:21:50 -0300

    se modifica el código de acuerdo a la arquitectura solicitada

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,26 +1,32 @@
-import { User } from '../dao/models/userModel.js';
+import User from '../dao/models/userModel.js';
 
 export class UserRepository {
+  // Listar todos los usuarios
   async getAll() {
-    return await User.find();
+    return await User.find().lean(); // lean() devuelve objetos JS planos
   }
 
+  // Buscar usuario por ID
   async getById(id) {
-    return await User.findById(id);
+    return await User.findById(id).lean();
   }
 
+  // Buscar usuario por email
   async getByEmail(email) {
-    return await User.findOne({ email });
+    return await User.findOne({ email }).lean();
   }
 
+  // Crear usuario
   async create(userData) {
     return await User.create(userData);
   }
 
+  // Actualizar usuario
   async update(id, updateData) {
     return await User.findByIdAndUpdate(id, updateData, { new: true });
   }
 
+  // Eliminar usuario
   async delete(id) {
     return await User.findByIdAndDelete(id);
   }
