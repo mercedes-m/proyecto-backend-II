3ded0bac98a650f676764dc818ac560ea4021b8b
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-02 16:56:16 -0300

    se crea el archivo de acuerdo a la arquitectura solicitada

---
+++ src/dtos/ProductDTO.js
@@ -0,0 +1,10 @@
+export class ProductDTO {
+  constructor(product) {
+    this.id = product._id || product.id;
+    this.title = product.title;
+    this.description = product.description;
+    this.price = product.price;
+    this.stock = product.stock;
+    this.category = product.category || null;
+  }
+}
