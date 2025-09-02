288b50f0098a75222458176b326690ae608900ed
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-01 21:10:29 -0300

    se modifica UserDTO.js

--- src/dtos/UserDTO.js
+++ src/dtos/UserDTO.js
@@ -4,5 +4,15 @@ export class UserDTO {
     this.last_name = user.last_name;
     this.email = user.email;
     this.role = user.role;
+
+    // Guardamos internamente la contraseña hasheada para validaciones
+    // No se enviará en la respuesta de la API
+    this._password = user.password;
+  }
+
+  // Método para serializar el DTO cuando se devuelve en la API
+  toJSON() {
+    const { _password, ...safeData } = this;
+    return safeData;
   }
 }
