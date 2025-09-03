24d6db6a13a423e66220803ac678f2fce7b8e9f7
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-03 19:33:48 -0300

    se corrige un error en el archivo

--- src/dao/cartDBManager.js
+++ src/dao/cartDBManager.js
@@ -67,7 +67,7 @@ class cartDBManager {
 
     async updateAllProducts(cid, products) {
 
-        // Validate if exist products
+        // Validar si el producto existe
         for (let key in products) {
             await this.productDBManager.getProductByID(products[key].product);
         }
