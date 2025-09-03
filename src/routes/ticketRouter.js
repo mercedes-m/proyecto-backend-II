e37c6191cd8f5e84b16e937e14c50c3fa58ea85b
Mercedes Muñoz <79.mariamercedes@gmail.com>
2025-09-03 19:38:57 -0300

    se corrige un error en la importación

--- src/routes/ticketRouter.js
+++ src/routes/ticketRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { TicketController } from '../controllers/ticketController.js';
 
-const router = Router();
+const ticketRouter = Router();
+const ticketController = new TicketController();
 
 // Endpoint para generar un ticket a partir del carrito
-router.post('/purchase/:cid', TicketController.purchaseCart);
+ticketRouter.post('/purchase/:cid', ticketController.purchaseCart.bind(ticketController));
 
-export default router;
+export default ticketRouter;
